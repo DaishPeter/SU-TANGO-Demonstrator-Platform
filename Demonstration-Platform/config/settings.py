55db4fb6e381ef1cdf8e3816c29d08636323a56a
peter <peterdaish16@gmail.com>
2025-09-29 12:09:52 +0100

    Landing Page Project

--- Demonstration-Platform/config/settings.py
+++ Demonstration-Platform/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+TEMPLATES[0]['DIRS'] = [BASE_DIR / "templates"]
+
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Optional for global static
